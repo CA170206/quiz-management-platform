dd8e023779a13b22c5a7135cccd516cae705b244
Chaitanya45 <chaitanyaanmulwar1702@gmail.com>
2026-08-07 12:08:54 +0530

    Connect Express with PostgreSQL database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 import app from "./app.js";
+import pool from "./config/db.js";
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+async function startServer() {
+    try {
+        await pool.query("SELECT NOW()");
+        console.log("✅ PostgreSQL Connected");
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on port ${PORT}`);
+        });
+
+    } catch (err) {
+        console.error("Database Connection Error");
+        console.error(err.message);
+    }
+}
+
+startServer();
